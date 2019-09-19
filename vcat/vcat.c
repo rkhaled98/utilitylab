3079d2e97536c203b3b76283732818a6f4e60ede
Rafi Khaled <rkhaled@vassar.edu>
2019-09-19 17:58:23 -0400

    cleanup

--- vcat/vcat.c
+++ vcat/vcat.c
@@ -3,40 +3,44 @@
 *
 * Your Name: Rafi Khaled
 *********************************************************/
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int i;
-    for (i=1; i < argc; i++){
+    for (i = 1; i < argc; i++)
+    {
         FILE *fp = fopen(argv[i], "r");
-        if (fp == NULL) {
-                printf("vcat: cannot open file\n");
-                exit(1);
-        }
-        else{
-            char *newline = malloc(300);
-            char *line;
-            while (1) {
-                line = fgets(newline, 300, fp);
 
-                if (line == NULL){
+        if (fp == NULL)
+        {
+            printf("vcat: cannot open file\n");
+            exit(1);
+        }
+        else
+        {
+            while (1)
+            {
+                // allocate 64 bytes
+                char *newline = (char *)malloc(64);
+
+                // read 64 characters at most per time
+                newline = fgets(newline, 64, fp);
+
+                if (newline == NULL)
+                {
                     break;
                 }
 
-                printf("%s", line);
-                free(newline);
-                newline = malloc(64);
-            }
+                printf("%s", newline);
 
+                free(newline);
+            }
         }
 
         fclose(fp);
-        
-        }
+    }
 
-        return 0;
-    
-
-    
+    return 0;
 }
