3e303628798a747391513a5b29d6e7e78258f08a
Rafi Khaled <rkhaled@vassar.edu>
2019-09-19 16:06:27 -0400

    working vcat tests passing

--- vcat/vcat.c
+++ vcat/vcat.c
@@ -3,15 +3,40 @@
 *
 * Your Name: Rafi Khaled
 *********************************************************/
-#include <stdio.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 
 int main(int argc, char *argv[]) {
-    FILE *fp = fopen("vcat.c", "r");
+    int i;
+    for (i=1; i < argc; i++){
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+                printf("vcat: cannot open file\n");
+                exit(1);
+        }
+        else{
+            char *newline = malloc(300);
+            char *line;
+            while (1) {
+                line = fgets(newline, 300, fp);
+
+                if (line == NULL){
+                    break;
+                }
+
+                printf("%s", line);
+                free(newline);
+                newline = malloc(64);
+            }
+
+        }
+
+        fclose(fp);
+        
+        }
+
+        return 0;
     
-    if (fp == NULL) {
-        printf("cannot open file\n");
-        // exit(1);
-    }
 
     
 }
